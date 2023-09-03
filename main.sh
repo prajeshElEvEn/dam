3ce6a6d3288fbce1df7e11f75bf244cc65de162a
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-09-03 20:06:38 +0530

    added qgc in bash script

--- main.sh
+++ main.sh
@@ -4,4 +4,8 @@ gnome-terminal -- bash -c 'gz sim -v4 -r iris_runway.sdf'
 
 sleep 5
 
-gnome-terminal -- bash -c 'sim_vehicle.py -v ArduCopter -f gazebo-iris --model JSON --map --console'
+gnome-terminal -- bash -c 'sim_vehicle.py -v ArduCopter -f gazebo-iris --model JSON --console'
+
+sleep 5
+
+gnome-terminal -- bash -c './scripts/automation/qgroundcontrol/QGroundControl.AppImage'
