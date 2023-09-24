1654f56b3565553a55765a9302308f0d956243fb
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-09-24 22:40:49 +0530

    created login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Button,
+  Container,
+  Grid,
+  IconButton,
+  InputAdornment,
+  Link,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import Brand from "../components/cards/Brand";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <Container>
+      <Stack height={{ xs: "auto", md: "100vh" }} justifyContent={"center"}>
+        <Grid
+          container
+          direction={{ xs: "column", md: "row" }}
+          rowSpacing={{ xs: 4, md: 1 }}
+          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        >
+          <Grid item xs={6}>
+            <Brand />
+          </Grid>
+          <Grid item xs={6}>
+            <Stack spacing={4}>
+              <Typography variant="h6">Login</Typography>
+              <Stack spacing={2}>
+                <TextField label="Email Id" type="email" variant="outlined" />
+                <TextField
+                  label="Password"
+                  type={showPassword ? "text" : "password"}
+                  variant="outlined"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showPassword ? (
+                            <VisibilityOffIcon />
+                          ) : (
+                            <VisibilityIcon />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <Button variant="contained">Login</Button>
+                <Typography>
+                  Do not have an account? <Link href="/">Sign Up</Link>{" "}
+                </Typography>
+              </Stack>
+            </Stack>
+          </Grid>
+        </Grid>
+      </Stack>
+    </Container>
+  );
 };
 
 export default Login;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import {
-  Avatar,
   Button,
   Container,
   Grid,
@@ -13,13 +12,7 @@ import {
 } from "@mui/material";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
-import DAMLogo from "../assets/images/logo.png";
-import ROSLogo from "../assets/images/ros.png";
-import QGCLogo from "../assets/images/qgc.png";
-import GazeboLogo from "../assets/images/gazebo.png";
-import WebODMLogo from "../assets/images/webodm.png";
-import ArdupilotLogo from "../assets/images/ardupilot.png";
-import AuthorLogo from "../assets/images/author.png";
+import Brand from "../components/cards/Brand";
 
 const Signup = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -40,38 +33,7 @@ const Signup = () => {
           columnSpacing={{ xs: 1, sm: 2, md: 3 }}
         >
           <Grid item xs={6}>
-            <Stack spacing={4}>
-              <Stack>
-                <Avatar
-                  variant="rounded"
-                  src={DAMLogo}
-                  sx={{ width: 100, height: 100 }}
-                />
-                <Typography variant="h4" mb={2}>
-                  DAM
-                </Typography>
-                <Typography variant="h6">
-                  Drone Automation and Mapping
-                </Typography>
-                <Typography variant="body1">
-                  Streamline drone operations, create detailed maps, and enhance
-                  object detection with our powerful software suite.
-                </Typography>
-              </Stack>
-              <Stack direction={"row"} spacing={2}>
-                <Avatar variant="rounded" src={ArdupilotLogo} />
-                <Avatar variant="rounded" src={ROSLogo} />
-                <Avatar variant="rounded" src={QGCLogo} />
-                <Avatar variant="rounded" src={GazeboLogo} />
-                <Avatar variant="rounded" src={WebODMLogo} />
-              </Stack>
-              <Stack direction={"row"} spacing={1} alignItems={"center"}>
-                <Avatar src={AuthorLogo} />
-                <Typography variant="body1">
-                  Made with love by Drone Scouts
-                </Typography>
-              </Stack>
-            </Stack>
+            <Brand />
           </Grid>
           <Grid item xs={6}>
             <Stack spacing={4}>
