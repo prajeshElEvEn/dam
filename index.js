590cdaa5f9ebb668d7119c788c9c7d3ee507bff6
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-10-12 23:10:44 +0530

    added constants and index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const { env, log, success, db } = require("./server/utils");
+const express = require("express");
+const cors = require("cors");
+const constants = require("./constants");
+
+env();
+
+const port = process.env.PORT || 5000;
+const hostname = process.env.HOSTNAME || "127.0.0.1";
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/uploads", express.static(constants.paths.uploadDir));
+
+app.listen(port, hostname, async () => {
+  await db();
+  success(`Server running at http://${hostname}:${port}`);
+});
