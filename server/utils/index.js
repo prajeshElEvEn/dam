a366fd12eee20c4e4403da0ccd5d7c7b372bb5ae
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-10-13 00:07:49 +0530

    added functions to utils

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,5 +1,11 @@
 const utils = module.exports;
 const { log, warn, error, success } = require("./logger");
+const {
+  generateToken,
+  generateResetToken,
+  sendResetEmail,
+  validateResetToken,
+} = require("./auth");
 
 utils.log = log;
 utils.success = success;
@@ -7,3 +13,7 @@ utils.warn = warn;
 utils.error = error;
 utils.env = require("./env");
 utils.db = require("./db");
+utils.generateToken = generateToken;
+utils.generateResetToken = generateResetToken;
+utils.sendResetEmail = sendResetEmail;
+utils.validateResetToken = validateResetToken;
