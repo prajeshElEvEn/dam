01cb96027c2cdd67c5875ec009b49bce2d57a122
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-10-14 03:33:42 +0530

    added: - navbar - sidebar - scroll to top btn - icons module - components module

--- client/src/assets/icons/index.js
+++ client/src/assets/icons/index.js
@@ -1,1 +1,23 @@
-export const icons = {};
+import MenuIcon from "@mui/icons-material/Menu";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
+import AutoAwesomeMosaicIcon from "@mui/icons-material/AutoAwesomeMosaic";
+import RadarIcon from "@mui/icons-material/Radar";
+import SettingsIcon from "@mui/icons-material/Settings";
+
+export {
+  MenuIcon,
+  InboxIcon,
+  MailIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  KeyboardArrowUpIcon,
+  AutoAwesomeIcon,
+  AutoAwesomeMosaicIcon,
+  RadarIcon,
+  SettingsIcon,
+};
--- client/src/components/layouts/RootLayout.js
+++ client/src/components/layouts/RootLayout.js
@@ -1,7 +1,39 @@
-import React from "react";
+import { useState } from "react";
+import { Outlet, ScrollRestoration } from "react-router-dom";
+import { Box, Fab } from "@mui/material";
+import { DrawerHeader } from "../styledComponents";
+import Navbar from "../bars/Navbar";
+import Sidebar from "../bars/Sidebar";
+import { KeyboardArrowUpIcon } from "../../assets/icons";
+import { ScrollTop } from "../utils";
 
 const RootLayout = () => {
-  return <div>RootLayout</div>;
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <ScrollRestoration />
+      <Navbar open={open} handleDrawerOpen={handleDrawerOpen} />
+      <Sidebar open={open} handleDrawerClose={handleDrawerClose} />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader id="back-to-top-anchor" />
+        <Outlet />
+      </Box>
+      <ScrollTop>
+        <Fab size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+    </Box>
+  );
 };
 
 export default RootLayout;
