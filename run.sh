5cbc6d9065a0defc2662794909daf9ba9ab59a2d
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-10-14 22:16:44 +0530

    added ROS2 integration with ardupilot

--- run.sh
+++ run.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-gnome-terminal -- bash -c 'gz sim -v4 -r iris_runway.sdf'
+gnome-terminal --tab --title="ROS2" -- bash -c 'echo "Launching simulation..."; sleep 2; source scripts/automation/install/setup.sh; sleep 5; ros2 launch ardupilot_gz_bringup iris_runway.launch.py'
 
-sleep 5
+gnome-terminal --tab --title="Ardupilot" -- bash -c 'echo "Launching SITL..."; sleep 2; sim_vehicle.py -v ArduCopter -f gazebo-iris --model JSON --console'
 
-gnome-terminal -- bash -c 'sim_vehicle.py -v ArduCopter -f gazebo-iris --model JSON --console'
-
-sleep 5
-
-gnome-terminal -- bash -c './scripts/utils/qgroundcontrol/QGroundControl.AppImage'
+gnome-terminal --tab --title="QGC" -- bash -c 'echo "Opening QGC..."; sleep 2; ./scripts/utils/qgroundcontrol/QGroundControl.AppImage'
