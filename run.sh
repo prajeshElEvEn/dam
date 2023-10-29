86fd41490ef0b7f129031128f2a41ea4cb8fb0c9
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-10-29 21:44:05 +0530

    added webodm

--- run.sh
+++ run.sh
@@ -4,8 +4,11 @@ gnome-terminal --tab --title="micro ROS" -- bash -c 'echo "Launching simulation.
 
 gnome-terminal --tab --title="Sim" -- bash -c 'echo "Launching simulation..."; sleep 2; source scripts/automation/install/setup.sh; sleep 5; ros2 launch ardupilot_gz_bringup iris_runway.launch.py'
 
-
 gnome-terminal --tab --title="QGC" -- bash -c 'echo "Opening QGC..."; sleep 2; ./scripts/utils/qgroundcontrol/QGroundControl.AppImage'
+
+gnome-terminal --tab --title="WebODM" -- bash -c 'echo "Launching WebODM..."; sleep 2; cd scripts/mapping/WebODM; ./webodm.sh start'
+
+gnome-terminal --tab --title="DAM" -- bash -c 'echo "Launching DAM..."; sleep 2; npm run dev'
 
 gnome-terminal --tab --title="Ardupilot" -- bash -c 'echo "Launching SITL..."; sleep 2; sim_vehicle.py -w -v ArduCopter -f gazebo-iris --model JSON --console -DG --enable-dds'
 
