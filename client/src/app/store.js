894252f8137fc9ea9883fc1c002e09abe3c031ac
Prajesh Pratap Singh <prajesh.eleven118@gmail.com>
2024-01-08 03:16:10 +0530

    added store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
-import userReducer from "../features/users/userSlice";
+import userReducer from "../features/user/userSlice";
 
 export const store = configureStore({
   reducer: {
