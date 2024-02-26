5251372080f9b0a20ac4161320d25dde6e6025a3
Prajesh Pratap Singh <prajesh.eleven118@gmail.com>
2024-02-26 11:01:07 +0530

    added ros script

--- setup.sh
+++ setup.sh
@@ -68,3 +68,21 @@ if [ ! -f "/etc/apt/sources.list.d/gazebo-stable.list" ]; then
 else
     echo "Gazebo already present."
 fi
+
+
+# install ros
+echo "> Installing ROS..."
+sleep 2
+if [ ! -f "/etc/apt/sources.list.d/ros-latest.list" ]; then
+    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+    sudo apt update
+    sudo apt install ros-noetic-desktop-full
+    echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+    source ~/.bashrc
+    sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+    sudo rosdep init
+    rosdep update
+else
+    echo "ROS already present."
+fi
